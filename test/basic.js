829e97aa5b4c0e1b208791b69f369ba6bf3c54d3
Surya Teja K <shanmukhateja@gmail.com>
2019-06-16 10:30:45 +0530

    update tests

--- test/basic.js
+++ test/basic.js
@@ -7,7 +7,7 @@ test('basic usage', function (t) {
   t.plan(3)
   var str = '0123456789'
   var stream = toStream(str)
-  toBlob(stream, 'text/plain', function (err, blob) {
+  toBlob.getBlob(stream, 'text/plain', function (err, blob) {
     t.error(err)
     toBuffer(blob, function (err, buf) {
       t.error(err)
@@ -20,7 +20,7 @@ test('basic usage (without mimeType)', function (t) {
   t.plan(3)
   var str = '0123456789'
   var stream = toStream(str)
-  toBlob(stream, function (err, blob) {
+  toBlob.getBlob(stream, function (err, blob) {
     t.error(err)
     toBuffer(blob, function (err, buf) {
       t.error(err)
@@ -33,11 +33,53 @@ test('stress test usage', function (t) {
   t.plan(3)
   var str = new Array(1000000).join('0123456789')
   var stream = toStream(str)
-  toBlob(stream, 'text/plain', function (err, blob) {
+  toBlob.getBlob(stream, 'text/plain', function (err, blob) {
     t.error(err)
     toBuffer(blob, function (err, buf) {
       t.error(err)
       t.equal(buf.toString(), str)
     })
   })
+})
+
+test('basic usage with Promise', function (t) {
+  var str = '0123456789'
+  var stream = toStream(str)
+  toBlob.getBlobPromise(stream, 'text/plain')
+    .then(function (blob) {
+      toBuffer(blob, function (err, buf) {
+        t.error(err)
+        t.equal(buf.toString(), str)
+        t.end(err)
+      })
+    })
+    .catch(function (err) { t.error(err) })
+})
+
+test('basic usage (without MimeType) with Promise', function (t) {
+  var str = '0123456789'
+  var stream = toStream(str)
+  toBlob.getBlobPromise(stream)
+    .then(function (blob) {
+      toBuffer(blob, function (err, buf) {
+        t.error(err)
+        t.equal(buf.toString(), str)
+        t.end(err)
+      })
+    })
+    .catch(function (err) { t.error(err) })
+})
+
+test('stress test usage with Promise', function (t) {
+  var str = new Array(1000000).join('0123456789')
+  var stream = toStream(str)
+  toBlob.getBlobPromise(stream, 'text/plain')
+    .then(function (blob) {
+      toBuffer(blob, function (err, buf) {
+        t.error(err)
+        t.equal(buf.toString(), str)
+        t.end(err)
+      })
+    })
+    .catch(function (err) { t.error(err) })
 })
