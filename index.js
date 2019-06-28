09226175dcac2ea67904b2e2f917d19bb36d50d8
Surya Teja K <shanmukhateja@gmail.com>
2019-06-28 19:56:25 +0530

    - removed callback interface - removed 'once' module dependency - updated README.md - updated tests

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
 /* global Blob */
 
-var once = require('once')
-
-module.exports = {
-  getBlob: function (stream, mimeType, cb) {
-    if (typeof mimeType === 'function') return this.getBlob(stream, null, mimeType)
-    cb = once(cb)
-    var chunks = []
-    stream
-      .on('data', function (chunk) {
-        chunks.push(chunk)
-      })
-      .on('end', function () {
-        var blob = mimeType
-          ? new Blob(chunks, { type: mimeType })
-          : new Blob(chunks)
-        cb(null, blob)
-      })
-      .on('error', cb)
-  },
-  getBlobPromise: function (stream, mimeType) {
-    return new Promise(function (resolve, reject) {
-      if (typeof mimeType === 'function') {
-        reject(new Error('Invalid mimetype, expected string, got function.'))
-      }
-      let chunks = []
-      stream.on('data', function (chunk) {
-        chunks.push(chunk)
-      })
-        .on('end', function () {
-          if (typeof mimeType === 'string') {
-            resolve(new Blob(chunks, { type: mimeType }))
-          } else {
-            resolve(new Blob(chunks))
-          }
-        })
-        .on('error', function (err) { reject(err) })
+module.exports = function (stream, mimeType) {
+  return new Promise(function (resolve, reject) {
+    if (typeof mimeType === 'function') {
+      reject(new Error('Invalid mimetype, expected string, got function.'))
+    }
+    let chunks = []
+    stream.on('data', function (chunk) {
+      chunks.push(chunk)
     })
-  }
+      .on('end', function () {
+        if (typeof mimeType === 'string') {
+          resolve(new Blob(chunks, { type: mimeType }))
+        } else {
+          resolve(new Blob(chunks))
+        }
+      })
+      .on('error', function (err) { reject(err) })
+  })
 }
--- test/basic.js
+++ test/basic.js
@@ -3,49 +3,10 @@ var toBlob = require('../')
 var toBuffer = require('blob-to-buffer')
 var toStream = require('string-to-stream')
 
-test('basic usage', function (t) {
-  t.plan(3)
-  var str = '0123456789'
-  var stream = toStream(str)
-  toBlob.getBlob(stream, 'text/plain', function (err, blob) {
-    t.error(err)
-    toBuffer(blob, function (err, buf) {
-      t.error(err)
-      t.equal(buf.toString(), str)
-    })
-  })
-})
-
-test('basic usage (without mimeType)', function (t) {
-  t.plan(3)
-  var str = '0123456789'
-  var stream = toStream(str)
-  toBlob.getBlob(stream, function (err, blob) {
-    t.error(err)
-    toBuffer(blob, function (err, buf) {
-      t.error(err)
-      t.equal(buf.toString(), str)
-    })
-  })
-})
-
-test('stress test usage', function (t) {
-  t.plan(3)
-  var str = new Array(1000000).join('0123456789')
-  var stream = toStream(str)
-  toBlob.getBlob(stream, 'text/plain', function (err, blob) {
-    t.error(err)
-    toBuffer(blob, function (err, buf) {
-      t.error(err)
-      t.equal(buf.toString(), str)
-    })
-  })
-})
-
 test('basic usage with Promise', function (t) {
   var str = '0123456789'
   var stream = toStream(str)
-  toBlob.getBlobPromise(stream, 'text/plain')
+  toBlob(stream, 'text/plain')
     .then(function (blob) {
       toBuffer(blob, function (err, buf) {
         t.error(err)
@@ -59,7 +20,7 @@ test('basic usage with Promise', function (t) {
 test('basic usage (without MimeType) with Promise', function (t) {
   var str = '0123456789'
   var stream = toStream(str)
-  toBlob.getBlobPromise(stream)
+  toBlob(stream)
     .then(function (blob) {
       toBuffer(blob, function (err, buf) {
         t.error(err)
@@ -73,7 +34,7 @@ test('basic usage (without MimeType) with Promise', function (t) {
 test('stress test usage with Promise', function (t) {
   var str = new Array(1000000).join('0123456789')
   var stream = toStream(str)
-  toBlob.getBlobPromise(stream, 'text/plain')
+  toBlob(stream, 'text/plain')
     .then(function (blob) {
       toBuffer(blob, function (err, buf) {
         t.error(err)
